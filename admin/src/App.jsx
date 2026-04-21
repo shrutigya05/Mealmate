28024c4063ef4eb6c0a92f98099795ca950b856d
Shrutigya Bhawsar <128145407+shrutigya05@users.noreply.github.com>
2026-04-22 00:17:56 +0530

    Update API URL to production endpoint

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://mealmate-3-q0iy.onrender.com"
 
 
   return (
